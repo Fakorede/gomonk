644586bb0f39409bb2409b68ab3d465d97f74015
Fakorede Abiola <abiolafakorede@gmail.com>
2024-12-15 12:54:39 -0600

    Add helper functions for tokens

--- token/token.go
+++ token/token.go
@@ -8,27 +8,42 @@ type Token struct {
 }
 
 const (
-  ILLEGAL = "ILLEGAL"
-  EOF = "EOF"
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
 
-  // Identifiers/literals
-  IDENT = "IDENT"
-  INT = "INT"
+	// Identifiers/literals
+	IDENT = "IDENT"
+	INT   = "INT"
 
-  // Operators
-  ASSIGN = "="
-  PLUS = "+"
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
 
-  // Delimiters
-  COMMA = ","
-  SEMICOLON = ";"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
 
-  LPAREN = "("
-  RPAREN = ")"
-  LBRACE = "{"
-  RBRACE = "}"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
-  // Keywords
-  FUNCTION = "FUNCTION"
-  LET = "LET"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
 )
+
+// GoMonk keywords table
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+// LookupIdent checks keywords table to see if a given identifer is a keyword.
+// Returns the keyword's constant if it is, otherwise returns token.IDENT.
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
