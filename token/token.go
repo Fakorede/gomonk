e845dc6a14a5fd8f9264b1a90b2218eba62087bb
Fakorede Abiola <abiolafakorede@gmail.com>
2024-12-16 19:11:14 -0600

    Gomonk lexer

--- token/token.go
+++ token/token.go
@@ -16,8 +16,18 @@ const (
 	INT   = "INT"
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+
+	LT = "<"
+	GT = ">"
+
+  EQ = "=="
+  NOT_EQ = "!="
 
 	// Delimiters
 	COMMA     = ","
@@ -31,12 +41,22 @@ const (
 	// Keywords
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
 )
 
 // GoMonk keywords table
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 // LookupIdent checks keywords table to see if a given identifer is a keyword.
